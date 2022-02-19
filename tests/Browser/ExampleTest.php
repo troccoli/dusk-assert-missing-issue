40a6160d4964f7b1872d722311ed9e128c18fc1a
Giulio Troccoli-Allard <giulio@troccoli.it>
2022-02-19 17:37:53 +0000

    testing

---
+++ tests/Browser/ExampleTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Browser;
+
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class ExampleTest extends DuskTestCase
+{
+    /**
+     * A basic browser test example.
+     *
+     * @return void
+     */
+    public function testBasicExample()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                ->screenshot('top')
+                ->assertVisible('#laravel-logo')
+                ->scrollTo('#footer')
+                ->screenshot('bottom')
+                ->assertMissing('#laravel-logo');
+        });
+    }
+}
